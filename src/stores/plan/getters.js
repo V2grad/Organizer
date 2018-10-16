87d3fbd2c94d2eb6cf0630123e454b8a59d7d7ef
NeverBehave <gayhub@never.pet>
2018-10-16 12:18:09 -0400

    WIP: fix actions and housekeeping

--- src/stores/plan/getters.js
+++ src/stores/plan/getters.js
@@ -2,60 +2,58 @@ import _ from 'lodash'
 import { CUSTOM_COURSE_TITLE, DEFAULT_YEAR, PERIOD } from '../const'
 
 export default {
-  getters: {
-    getSemesterTree: (state) => {
-      let tree = {}
-      state.semesters.forEach((s, i) => {
-        if (tree[s.year] === undefined) {
-          tree[s.year] = {}
-        }
-        tree[s.year][s.period] = i
-      })
-      return tree
-    },
-    getStartYear: (state) => {
-      // Maybe just let them choose whatever they like.
-      return DEFAULT_YEAR
-    },
-    getExistedPeriod: (state) => (year) => {
-      if (state.semesters[year] !== undefined) {
-        return _.keys(state.semesters[year])
+  getSemesterTree: (state) => {
+    let tree = {}
+    state.semesters.forEach((s, i) => {
+      if (tree[s.year] === undefined) {
+        tree[s.year] = {}
       }
-      return []
-    },
-    getSelectablePeriod: (state) => (year) => {
-      if (state.semesters[year] !== undefined) {
-        let arr = PERIOD
-        _.pullAll(arr, _.keys(state.semesters[year]))
-        return arr
-      }
-      return PERIOD
-    },
-    findCourse: (state) => (courseTitle) => {
-      // Even though course should appear only once, we collect every possible answers.
-      if (courseTitle.include(CUSTOM_COURSE_TITLE)) {
-        // Igore Custom Course
-        return []
-      } else {
-        let semesters = []
-        state.semesters.forEach((s) => {
-          let index = _.findIndex(s, ['CourseTitle', courseTitle])
-          if (index !== -1) {
-            semesters.push(index)
-          }
-        })
-        return semesters
-      }
-    },
-    findSemester: (state) => (semester) => {
-      return _.findIndex(state.semesters, semester)
-    },
-    allCourses: (state) => {
-      let courses = []
-      state.semesters.forEach((s) => {
-        courses = s.courses.concat(s.courses)
-      })
-      return courses
+      tree[s.year][s.period] = i
+    })
+    return tree
+  },
+  getStartYear: (state) => {
+    // Maybe just let them choose whatever they like.
+    return DEFAULT_YEAR
+  },
+  getExistedPeriod: (state) => (year) => {
+    if (state.semesters[year] !== undefined) {
+      return _.keys(state.semesters[year])
     }
+    return []
+  },
+  getSelectablePeriod: (state) => (year) => {
+    if (state.semesters[year] !== undefined) {
+      let arr = PERIOD
+      _.pullAll(arr, _.keys(state.semesters[year]))
+      return arr
+    }
+    return PERIOD
+  },
+  findCourse: (state) => (courseTitle) => {
+    // Even though course should appear only once, we collect every possible answers.
+    if (courseTitle.include(CUSTOM_COURSE_TITLE)) {
+      // Igore Custom Course
+      return []
+    } else {
+      let semesters = []
+      state.semesters.forEach((s) => {
+        let index = _.findIndex(s, ['CourseTitle', courseTitle])
+        if (index !== -1) {
+          semesters.push(index)
+        }
+      })
+      return semesters
+    }
+  },
+  findSemester: (state) => (semester) => {
+    return _.findIndex(state.semesters, semester)
+  },
+  allCourses: (state) => {
+    let courses = []
+    state.semesters.forEach((s) => {
+      courses = s.courses.concat(s.courses)
+    })
+    return courses
   }
 }
--- src/stores/plan/mutations.js
+++ src/stores/plan/mutations.js
@@ -1,31 +1,29 @@
 import Vue from 'vue'
 
 export default {
-  mutations: {
-    // {semester: {}, index: Number}
-    addSemester (state, payload) {
-      state.semesters.splice(payload.index, 0, payload.semester)
-    },
-    removeSemester (state, index) {
-      Vue.delete(state.semesters, index)
-    },
-    // {semester: Index of Semester (Number), course: {}}
-    addCourse (state, payload) {
-      state.semetsers[payload.semester].courses.push(payload.course)
-    },
-    // {semester: Index of Semester (Number), course: Index of Course (Number)}
-    removeCourse (state, payload) {
-      state.semesters[payload.semester].courses.splice(payload.course)
-    },
-    updateName (state, name) {
-      state.name = name
-    },
-    // {semester: Index of Semester (Number), course: []}
-    updateCourses (state, payload) {
-      state.semesters[payload.semester].courses = payload.course
-    },
-    cleanPlan (state) {
-      state.semesters = []
-    }
+  // {semester: {}, index: Number}
+  addSemester (state, payload) {
+    state.semesters.splice(payload.index, 0, payload.semester)
+  },
+  removeSemester (state, index) {
+    Vue.delete(state.semesters, index)
+  },
+  // {semester: Index of Semester (Number), course: {}}
+  addCourse (state, payload) {
+    state.semetsers[payload.semester].courses.push(payload.course)
+  },
+  // {semester: Index of Semester (Number), course: Index of Course (Number)}
+  removeCourse (state, payload) {
+    state.semesters[payload.semester].courses.splice(payload.course)
+  },
+  updateName (state, name) {
+    state.name = name
+  },
+  // {semester: Index of Semester (Number), course: []}
+  updateCourses (state, payload) {
+    state.semesters[payload.semester].courses = payload.course
+  },
+  cleanPlan (state) {
+    state.semesters = []
   }
 }
