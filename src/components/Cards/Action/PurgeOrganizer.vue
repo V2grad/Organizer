ab0027f508fec63e0311b10c81e486f7ba06f5fe
NeverBehave <gayhub@never.pet>
2018-10-27 22:04:20 -0400

    Ref: show organizer version

--- src/components/Cards/Action/PurgeOrganizer.vue
+++ src/components/Cards/Action/PurgeOrganizer.vue
@@ -6,6 +6,9 @@
     <p class="card-text">
       Sometimes Organizer may not work properly as the data store in your browser is somewhat broken. Try resetting everything to get back to the right track.
     </p>
+    <p class="card-text">
+      Version: {{ this.$unify.VERSION }}
+    </p>
     <b-button
       v-b-modal="'_ActionPurgeOrganizer'"
       variant="danger"
--- src/utils/unify.js
+++ src/utils/unify.js
@@ -5,22 +5,28 @@ export default {
     }
     return 'Transferred Course'
   },
+
   CUSTOM_COURSE_PREFIX: 'CTSM-',
   CUSTOM_COURSE_TITLE: 'CTSM-1100',
+
   PLAN_MAXLENGTH: 50,
   COURSE_MAXLENGTH: 50,
+
   PERIOD: ['Spring', 'Summer', 'Fall'],
   COURSEATTRIBUTES: {
     CourseTitle: 'Course Title',
     CourseName: 'Course Name',
     CreditHours: 'Credit Hours'
   },
+
   DEFAULT_YEAR: (new Date()).getFullYear() - 5,
+  YEAR_SPAN: 15,
+
   TRANSFERRED_SEMESTER_INDEX: -1,
   TRANSFERRED_SEMESTER_OPTION: {
     text: 'Transferred Course',
     value: -1
   },
-  YEAR_SPAN: 15,
+
   VERSION: "1.00"
 }
