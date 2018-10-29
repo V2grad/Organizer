2e5e76501b127b8203ab4433577e12553a42e0da
NeverBehave <gayhub@never.pet>
2018-10-29 00:23:40 -0400

    Fix: SPA setting

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,18 @@ module.exports = {
   configureWebpack: {
     plugins: [
       new OfflinePlugin({
+        // https://github.com/NekR/offline-plugin/blob/master/docs/examples/SPA.md
+        // Unless specified in webpack's configuration itself
+        publicPath: '/',
+        appShell: '/',
+        externals: [
+          '/'
+        ],
+        // AppCache: {
+        //   FALLBACK: {
+        //     '/': '/offline-page.html'
+        //   }
+        // },
         excludes: ['**/.*', '**/_*', '**/*.map', '**/*.gz']
       })
     ]
